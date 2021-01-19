82b7e569bc635e4d7b5615576611f6800f2e440f
jquaglia <jqugali@gmail.com>
2021-01-18 19:53:23 -0800

    returned an object with necessary info on client request

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// ===packages===
+// ====== packages ======
 
 const express = require('express');
 
@@ -8,22 +8,58 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+
+// ====== setup the application ======
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+// ====== Other Global Variables ======
+const PORT = process.env.PORT || 3111;
+
+// ====== Routes ======
+
+app.get('/', (request, response) => {
+  response.send('you made it home, party time');
+});
+
+
+
+app.get('/location', (request, response) => {
+  if (request.query.city === ''){
+    response.status(500).send('Error pick a city to explore')
+    return;
+  }
+
+  // ====== Normalize Data ======
+  const theDataArrayFromTheLocationJson = require('./data/location.json');
+  const theDataObjFromJson = theDataArrayFromTheLocationJson[0];
+  
+  // ====== Data from the client ======
+  console.log('request.query', request.query)
+  console.log('request.query.city', request.query.city)
+  const searchedCity = request.query.city;
+
+  const newLocation = new Location(
+    searchedCity,
+    theDataObjFromJson.display_name,
+    theDataObjFromJson.lat,
+    theDataObjFromJson.lon
+  );
+    console.log('newLocation', newLocation);
+  response.send(newLocation);
+});
 
 
 
 
-
-
-
-
-
-
-
+// ====== Helper Funtions ======
+function Location(search_query, formatted_query, latitude, longitude){
+  this.search_query = search_query;
+  this.formatted_query = formatted_query;
+  this.longitude = longitude;
+  this.latitude = latitude;
+}
 
 
 // ====== Start the server ======
-app.listen(PORT, () => {console.log('server is listening')});
+app.listen(PORT, () => {console.log(`server is listening on Port ${PORT}`)});
