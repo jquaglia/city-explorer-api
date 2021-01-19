b8d98df107832c6da582a444f72feb6a84b2e2aa
jquaglia <jqugali@gmail.com>
2021-01-18 17:29:57 -0800

    completed initializing repo

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 'use strict';
 
+// ===packages===
+
+const express = require('express');
+
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ====== Start the server ======
+app.listen(3000, () => {console.log('server is listening')});
